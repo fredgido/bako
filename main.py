658e6f25e296eeef69c961276f44d54657780348
fredgido <fredgido@gmail.com>
2019-08-26 01:20:20 +0100

    multiple

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ from flask import render_template
 
 app = Flask(__name__)
 
-posts = "https://raikou4.donmai.us/preview/b6/1d/b61d520fc98495185487ab3af37407f9.jpg"
+posts = ["https://raikou4.donmai.us/preview/b6/1d/b61d520fc98495185487ab3af37407f9.jpg","https://raikou4.donmai.us/preview/ff/70/ff7032ecc5aa8bbe529a335842ac7463.jpg","https://raikou4.donmai.us/preview/ed/2f/ed2fd218b14ba9f6fc9b04f358d3b9c8.jpg","https://raikou4.donmai.us/preview/11/b2/11b2741bb1257c41a71ed0802f4b79bf.jpg"]
 
 @app.route('/')
 def hello_world():
